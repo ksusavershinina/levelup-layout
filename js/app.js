38846208caca391bab47fe17db8d8c7354aff172
ksusavershinina <vershinina.ka@students.dvfu.ru>
2023-05-02 17:00:28 +1000

    add normal responsive to reg-log

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const container = document.querySelector(".container");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.add("sign-in-mode");
@@ -8,7 +10,17 @@ sign_in_btn.addEventListener("click", () => {
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.remove("sign-in-mode");
-})
+});
+
+sign_in_btn2.addEventListener("click", () => {
+  container.classList.add("sign-in-mode2");
+});
+
+sign_up_btn2.addEventListener("click", () => {
+  container.classList.remove("sign-in-mode2");
+});
+
+
 
 const checkbox = document.querySelector('.checkbox-default');
 const checkboxText = document.querySelector('.checkbox-text');
